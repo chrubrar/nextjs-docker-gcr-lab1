50ef634d98505c70a5ddb3c6efbd930203b357e3
Chru Brar <chrubrar@gmail.com>
2023-10-14 08:25:27 -0400

    adding heading for taigy

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>hello GCP Docker Lab</h1><br />
-        <h3>We meet again</h3>
+        <h3>We meet again</h3> <br />
+        <h3>Taigy is the best</h3>
       </div>
     </main>
   )
