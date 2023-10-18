abbf5c30943986c168d6d3e12e2670a09c668a74
Chru Brar <chrubrar@gmail.com>
2023-10-18 11:19:11 -0400

    test ci/cd pipeline 1/18/1

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>hello GCP Docker Lab</h1><br />
+        <h1>hello GCP Docker Lab</h1>
+        <br />
         <h3>We meet again</h3> <br />
-        <h3>Taigy is the best</h3>< br />
+        <h3>Taigy is the best</h3>
+        <br />
+        <h3>Taigy is the best</h3>
+        <br />
+        <h3>Taigy is the bestest</h3>
+        <br />
         <Link href="https://www.cnn.com">CNN</Link>
       </div>
     </main>
-  )
+  );
 }
